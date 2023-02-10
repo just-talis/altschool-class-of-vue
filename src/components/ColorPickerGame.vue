9035163f43004d6ab82b06c3423d18178b9fb912
Esther Simon <esthersimon540@gmail.com>
2023-02-10 11:13:39 +0100

    matchColour

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -8,29 +8,21 @@
   </button>
 </template>
 
-<script>
+<script setup>
 import { ref, reactive } from "@vue/reactivity";
+import { useMatchColour } from "../composables/useMatchColour";
 
-export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
 
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+const colors = ["green", "red", "blue", "purple"];
+const { message, matchColor } = useMatchColour(colors);
 
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
-  },
-};
+//export default {
+//setup() {
+// const colors = ["green", "red", "blue", "purple"];
+//const {message, matchColor} = useMatchColour(colors);
+// return { colors, message, matchColor };
+//},
+//};
 </script>
 
 <style></style>
